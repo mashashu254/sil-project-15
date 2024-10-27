ae67343e59ac6be79611779498a46ddebf192630
magvtv <magutujr@gmail.com>
2024-10-27 12:10:06 +0300

    feat: basic authentication page css styling

--- src/components/login.js
+++ src/components/login.js
@@ -1,6 +1,6 @@
 import "../App.css";
-import axios from "axios";
-import { useState, useEffect } from "react";
+// import axios from "axios";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 // axios.defaults.xsrfCookieName = "csrftoken";
@@ -8,51 +8,50 @@ import { useNavigate } from "react-router-dom";
 // axios.defaults.withCredentials = true;
 
 function Login() {
-  const [currentUser, setCurrentUser] = useState(false);
-  const [username, setUsername] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+	// const [currentUser, setCurrentUser] = useState(false);
+	const [username, setUsername] = useState("");
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const navigate = useNavigate();
 
-  const navigate = useNavigate();
-
-  return (
-    <div className="center">
-      <h1>Login</h1>
-      <form onSubmit={() => navigate("/quiz")}>
-        <div>
-          <label>Username:</label>
-          <input
-            type="text"
-            id="username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
-        </div>
-        <div>
-          <label>Email:</label>
-          <input
-            type="email"
-            id="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </div>
-        <div>
-          <label>Password:</label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-        <button onClick={() => navigate("/quiz")}>Submit</button>
-        <p className="mb-3">
-          Don't have an account? <a href="/register">Register</a>
-        </p>
-      </form>
-    </div>
-  );
+	return (
+		<div className="center">
+			<h1>Login</h1>
+			<form onSubmit={() => navigate("/quiz")}>
+				<div className="user__input">
+					<label>Username:</label>
+					<input
+						type="text"
+						id="username"
+						value={username}
+						onChange={(e) => setUsername(e.target.value)}
+					/>
+				</div>
+				<div className="user__input">
+					<label>Email:</label>
+					<input
+						type="email"
+						id="email"
+						value={email}
+						onChange={(e) => setEmail(e.target.value)}
+					/>
+				</div>
+				<div className="user__input">
+					<label>Password:</label>
+					<input
+						type="password"
+						id="password"
+						value={password}
+						onChange={(e) => setPassword(e.target.value)}
+					/>
+				</div>
+				<button onClick={() => navigate("/quiz")}>Submit</button>
+				<p className="register">
+					Don't have an account? <a href="/register">Register</a>
+				</p>
+			</form>
+		</div>
+	);
 }
 
 export default Login;
--- src/components/register.js
+++ src/components/register.js
@@ -1,58 +1,48 @@
-import "../App.css";
-import axios from "axios";
-import { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import '../App.css';
+// import axios from "axios";
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 // axios.defaults.xsrfCookieName = "csrftoken";
 // axios.defaults.xsrfHeaderName = "X-CSRFToken";
 // axios.defaults.withCredentials = true;
 
 function Register() {
-  const [currentUser, setCurrentUser] = useState(false);
-  const [username, setUsername] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+	// const [currentUser, setCurrentUser] = useState(false);
+	const [username, setUsername] = useState('');
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
 
-  const navigate = useNavigate();
+	const navigate = useNavigate();
 
-  return (
-    <div className="center">
-      <h1>Register</h1>
-      <form onSubmit={() => navigate("/quiz")}>
-        <div>
-          <label>Username:</label>
-          <input
-            type="text"
-            id="username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
-        </div>
-        <div>
-          <label>Email:</label>
-          <input
-            type="email"
-            id="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </div>
-        <div>
-          <label>Password:</label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-        <button onClick={() => navigate("/quiz")}>Submit</button>
-        <p className="mb-3">
-          Already have an account? <a href="/login">Login</a>
-        </p>
-      </form>
-    </div>
-  );
+	return (
+		<div className="center">
+			<h1>Register</h1>
+			<form onSubmit={() => navigate('/quiz')}>
+				<div>
+					<label>Username:</label>
+					<input type="text" id="username" value={username} onChange={(e) => setUsername(e.target.value)} />
+				</div>
+				<div>
+					<label>Email:</label>
+					<input type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+				</div>
+				<div>
+					<label>Password:</label>
+					<input
+						type="password"
+						id="password"
+						value={password}
+						onChange={(e) => setPassword(e.target.value)}
+					/>
+				</div>
+				<button onClick={() => navigate('/quiz')}>Submit</button>
+				<p className="login">
+					Already have an account? <a href="/login">Login</a>
+				</p>
+			</form>
+		</div>
+	);
 }
 
 export default Register;
