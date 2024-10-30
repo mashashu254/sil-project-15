8147d4ea234ab10d143665a5095fef4aa890877e
magvtv <magutujr@gmail.com>
2024-10-30 18:40:32 +0300

    refactor(components): consolidate plan and advice display

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import './App.css';
+import './styles/index.css';
 import Routes from './routes';
 import reportWebVitals from './reportWebVitals';
 
